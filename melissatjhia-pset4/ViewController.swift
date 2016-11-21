7807cfbe0a517b9adaa6b205cf80d59439be275a
Melissa Tjhia <melissatjhia@wcw-stud-145-109-30-125.wireless.uva.nl>
2016-11-21 13:00:08 +0100

    Initial Commit

---
+++ melissatjhia-pset4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  melissatjhia-pset4
+//
+//  Created by Melissa Tjhia on 21-11-16.
+//  Copyright © 2016 Melissa Tjhia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
